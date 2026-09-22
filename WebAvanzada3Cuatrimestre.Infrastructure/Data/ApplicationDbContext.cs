03efb14c9fb84d3619c6db6e5a80f9802b73efd0
RichardArce <richard.arce.vargas@gmail.com>
2026-09-21 20:49:47 -0600

    Ajuste para inicio del sistema

--- WebAvanzada3Cuatrimestre.Infrastructure/Data/ApplicationDbContext.cs
+++ WebAvanzada3Cuatrimestre.Infrastructure/Data/ApplicationDbContext.cs
@@ -20,10 +20,6 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<Duenno> Duennos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=C:\\Users\\richa\\Desktop\\Proyectos de Clase\\WebAvanzada3Cuatrimestre\\WebAvanzada3Cuatrimestre.Infrastructure\\lavacar.db");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Carro>(entity =>
--- WebAvanzada3Cuatrimestre/Program.cs
+++ WebAvanzada3Cuatrimestre/Program.cs
@@ -7,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICarroRepository, CarroRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,12 +21,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-builder.Services.AddScoped<ICarroRepository, CarroRepository>();
-
 app.UseHttpsRedirection();
 app.UseRouting();
 
